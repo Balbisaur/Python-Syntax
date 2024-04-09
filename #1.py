09f4b4119fddc352bd51aea36b3a71964588a128
Balbisaur <albi3479@yahoo.com>
2024-04-09 13:08:27 -0500

    first commit

---
+++ #1.py
@@ -0,0 +1,14 @@
+weather = "sunny"
+
+if weather == "sunny":
+    print("wear sunglasses")
+else:
+    print("take an umbrella")
+
+
+mood = "happy"
+
+if mood == "happy":
+    print("thats great to hear")
+else:
+    print("I hope yoour day gets better")
